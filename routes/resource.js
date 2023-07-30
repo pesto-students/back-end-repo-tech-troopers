2c99b7e26d7792033636161338464855f49de4a7
ashudeo4 <ashudeo4@gmail.com>
2023-07-30 10:30:07 +0530

    added update resource api

--- routes/resource.js
+++ routes/resource.js
@@ -6,17 +6,26 @@ const Address = require("../models/Address");
 const { BadRequest } = require("../utils/errors");
 const loginMiddleware = require("../middlewares/auth");
 const { saveImage } = require("../config/cloudinary");
-const { phoneNumberValidation, checkForInvalid, emailValidation } = require("../utils/util");
+const {
+    phoneNumberValidation,
+    checkForInvalid,
+    emailValidation,
+} = require("../utils/util");
 const multer = require("multer");
 require("dotenv").config();
 
 const upload = multer({ dest: "uploads/" });
 const { ACTIVE } = require("../utils/constants");
-router.post('/', loginMiddleware, async (req, res) => {
-    const { category, imagePath, description, status, phoneNumber, email, address } = req.body;
-    if (!Boolean(imagePath)) {
-        throw new BadRequest("Image path is required");
-    }
+router.post("/", loginMiddleware, async (req, res) => {
+    const {
+        category,
+        imagePath,
+        description,
+        phoneNumber,
+        email,
+        address,
+    } = req.body;
+
     if (!Boolean(description)) {
         throw new BadRequest("Description is required");
     }
@@ -35,22 +44,26 @@ router.post('/', loginMiddleware, async (req, res) => {
     if (address) {
         const { addressLine1, city, state, pinCode } = address;
 
-        if (checkForInvalid(addressLine1) || checkForInvalid(city) || checkForInvalid(state) || checkForInvalid(pinCode)) {
-
+        if (
+            checkForInvalid(addressLine1) ||
+            checkForInvalid(city) ||
+            checkForInvalid(state) ||
+            checkForInvalid(pinCode)
+        ) {
             if (checkForInvalid(addressLine1)) {
-                throw new BadRequest(`Address is required.`)
+                throw new BadRequest(`Address is required.`);
             }
 
             if (checkForInvalid(city)) {
-                throw new BadRequest(`City is required.`)
+                throw new BadRequest(`City is required.`);
             }
 
             if (checkForInvalid(state)) {
-                throw new BadRequest(`State is required.`)
+                throw new BadRequest(`State is required.`);
             }
 
             if (checkForInvalid(pinCode)) {
-                throw new BadRequest(`Pincode is required.`)
+                throw new BadRequest(`Pincode is required.`);
             }
         }
     }
@@ -64,46 +77,122 @@ router.post('/', loginMiddleware, async (req, res) => {
         email,
         userId: req.user._id,
         address: savedAddress._id,
-        imageId
-    }
+        imageId,
+    };
 
     const savedResource = await Resource.create(resource);
-    await User.findByIdAndUpdate(req.user._id, { "$push": { resources: savedResource._id } });
+    await User.findByIdAndUpdate(req.user._id, {
+        $push: { resources: savedResource._id },
+    });
     return res.status(201).json(savedResource);
 });
 
-router.post("/upload", loginMiddleware, upload.array("image"), async (req, res) => {
-    try {
-        return res.status(201).json({ path: req.files[0].path });
-    } catch (err) {
-        console.log(err);
+router.post(
+    "/upload",
+    loginMiddleware,
+    upload.array("image"),
+    async (req, res) => {
+        try {
+            return res.status(201).json({ path: req.files[0].path });
+        } catch (err) {
+            console.log(err);
+        }
     }
-})
+);
 
-router.get("/", async (req, res) => {
+router.get("/", loginMiddleware, async (req, res) => {
     try {
         let page = req.query.page;
         let limit = req.query.limit;
 
         if (!page) {
-            page = 1
+            page = 1;
         }
         if (!limit) {
-            limit = 10
+            limit = 10;
         }
-        const resourceList = await Resource.find({status: ACTIVE}).limit(limit * 1).skip((page - 1) * limit).exec();
+        const resourceList = await Resource.find({ status: ACTIVE })
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .populate("address")
+            .exec();
         const totalCount = await Resource.countDocuments();
 
         return res.status(200).json({
             resourceList,
             totalPages: Math.ceil(totalCount / limit),
-            currentPage: page
+            currentPage: page,
         });
     } catch (err) {
         console.log(err);
     }
-})
+});
 
+router.put("/:resourceId", loginMiddleware, async (req, res) => { 
+    const {
+        category,
+        imagePath,
+        description,
+        phoneNumber,
+        email,
+        address,
+    } = req.body;
+    if (!Boolean(description)) {
+        throw new BadRequest("Description is required");
+    }
+    if (!Boolean(phoneNumber)) {
+        throw new BadRequest("Phone Number is required");
+    }
+    if (!phoneNumberValidation(phoneNumber)) {
+        throw new BadRequest("Enter valid phone number");
+    }
+    if (!Boolean(email)) {
+        throw new BadRequest("Email is required");
+    }
+    if (!emailValidation(email)) {
+        throw new BadRequest("Enter valid email");
+    }
+    if (address) {
+        const { addressLine1, city, state, pinCode } = address;
 
+        if (
+            checkForInvalid(addressLine1) ||
+            checkForInvalid(city) ||
+            checkForInvalid(state) ||
+            checkForInvalid(pinCode)
+        ) {
+            if (checkForInvalid(addressLine1)) {
+                throw new BadRequest(`Address is required.`);
+            }
+
+            if (checkForInvalid(city)) {
+                throw new BadRequest(`City is required.`);
+            }
+
+            if (checkForInvalid(state)) {
+                throw new BadRequest(`State is required.`);
+            }
+
+            if (checkForInvalid(pinCode)) {
+                throw new BadRequest(`Pincode is required.`);
+            }
+        }
+    }
+    const resourceData = await Resource.findById(req.params.resourceId);
+    const addressId = resourceData.address;
+    await Address.findByIdAndUpdate(addressId,address);
+    const resource = {
+        category,
+        description,
+        phoneNumber,
+        email,
+    };
+    if (Boolean(imagePath)) {
+        const imageId = await saveImage(imagePath);
+        resource["imageId"] = imageId;
+    }
+    const savedResource = await Resource.findByIdAndUpdate(resourceData._id, resource);
+    return res.status(200).json(savedResource);
+});
 
 module.exports = router;
