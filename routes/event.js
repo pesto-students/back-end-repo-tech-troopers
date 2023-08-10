9c606236004ebccc4d80b02ca196fa468b9bf6f2
ashudeo4 <ashudeo4@gmail.com>
2023-08-11 02:43:08 +0530

    added event edit api

--- routes/event.js
+++ routes/event.js
@@ -90,6 +90,9 @@ router.post('/', loginMiddleware, async (req, res, next) => {
 
 router.get("/", loginMiddleware, async (req, res, next) => {
     try {
+        if (req.user.role !== NGO_USER) {
+            throw new BadRequest("You are not allowed");
+        }
         let page = req.query.page;
         let limit = req.query.limit;
 
@@ -115,4 +118,85 @@ router.get("/", loginMiddleware, async (req, res, next) => {
     }
 });
 
+router.put("/:eventId",loginMiddleware,async(req,res,next)=>{
+    try{
+        const {
+            title,
+            description,
+            status,
+            organizedBy,
+            ngoName,
+            date,
+            address
+        } = req.body;
+
+        if (req.user.role !== NGO_USER) {
+            throw new BadRequest("You are not allowed");
+        }
+        if (!Boolean(title)) {
+            throw new BadRequest("Title is required");
+        }
+        if (!Boolean(description)) {
+            throw new BadRequest("Description is required");
+        }
+        if (!Boolean(status)) {
+            throw new BadRequest("Status is required");
+        }
+        if (!Boolean(organizedBy)) {
+            throw new BadRequest("Organized by is required");
+        }
+        if (!Boolean(ngoName)) {
+            throw new BadRequest("NGO name is required");
+        }
+        if (!Boolean(date)) {
+            throw new BadRequest("Date is required");
+        }
+        const { addressLine1, city, state, pinCode } = address;
+
+        if (
+            checkForInvalid(addressLine1) ||
+            checkForInvalid(city) ||
+            checkForInvalid(state) ||
+            checkForInvalid(pinCode)
+        ) {
+            if (checkForInvalid(addressLine1)) {
+                throw new BadRequest(`Address is required.`);
+            }
+
+            if (checkForInvalid(city)) {
+                throw new BadRequest(`City is required.`);
+            }
+
+            if (checkForInvalid(state)) {
+                throw new BadRequest(`State is required.`);
+            }
+
+            if (checkForInvalid(pinCode)) {
+                throw new BadRequest(`Pincode is required.`);
+            }
+        }
+        let eventData = await Event.findById(req.params.eventId);
+        if (!eventData) {
+            throw new BadRequest("Event not found");
+        }
+        const addressData = await Address.findById(eventData.address);
+        addressData.addressLine1 = addressLine1;
+        addressData.city = city;
+        addressData.state = state;
+        addressData.pinCode = pinCode;
+        await addressData.save();
+
+        eventData.title = title;
+        eventData.description = description;
+        eventData.status = (status == ACTIVE ? ACTIVE : IN_ACTIVE);
+        eventData.organizedBy = organizedBy;
+        eventData.ngoName = ngoName;   
+        eventData.date= new Date(date);
+        await eventData.save();
+
+        return res.status(200).json({message: "Successfully update events"});
+    }catch(err){
+        next(err);
+    }
+})
 module.exports = router;
