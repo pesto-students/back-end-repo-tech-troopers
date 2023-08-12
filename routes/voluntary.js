0f4d51c26d3fa06a1d1716b4f5a1ed806265cdf9
ashudeo4 <ashudeo4@gmail.com>
2023-08-12 19:38:47 +0530

    added listing api for admin and user

--- routes/voluntary.js
+++ routes/voluntary.js
@@ -5,7 +5,7 @@ const NGODetails = require("../models/NGODetails");
 const Voluntary = require("../models/Voluntary");
 const { BadRequest } = require("../utils/errors");
 const loginMiddleware = require("../middlewares/auth");
-const { ACCEPTED, ACTIVE, NGO_USER } = require("../utils/constants");
+const { ACCEPTED, ACTIVE, NGO_USER, USER } = require("../utils/constants");
 router.post("/admin", loginMiddleware, async (req, res, next) => {
     try {
         const {
@@ -20,7 +20,7 @@ router.post("/admin", loginMiddleware, async (req, res, next) => {
             registrationNumber,
             typeOfNGO,
         } = req.body;
-        console.log({user:req.user});
+
         if (req.user.role !== NGO_USER) {
             throw new BadRequest("You are not allowed");
         }
@@ -76,6 +76,66 @@ router.post("/admin", loginMiddleware, async (req, res, next) => {
         const voluntaryData = await Voluntary.create(newVoluntary);
         await User.findByIdAndUpdate(req.user._id, { $push: { voluntary: voluntaryData._id } });
         return res.status(201).json(newVoluntary);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/admin', loginMiddleware, async(req, res, next)=>{
+    try {
+        if (req.user.role !== NGO_USER) {
+            throw new BadRequest("You are not allowed");
+        }
+        let page = req.query.page;
+        let limit = req.query.limit;
+
+        if (!page) {
+            page = 1;
+        }
+        if (!limit) {
+            limit = 10;
+        }
+        const voluntaryList = await Voluntary.find({ userIdNGO: req.user._id })
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .populate("ngoDetailId")
+            .exec();
+        const totalCount = await Voluntary.countDocuments();
+        return res.status(200).json({
+            voluntaryList,
+            totalPages: Math.ceil(totalCount / limit),
+            currentPage: page,
+        });
+    } catch(err) {
+        next(err);
+    }
+});
+
+router.get('/', loginMiddleware, async (req, res, next) => {
+    try {
+        if (req.user.role !== USER) {
+            throw new BadRequest("You are not allowed");
+        }
+        let page = req.query.page;
+        let limit = req.query.limit;
+
+        if (!page) {
+            page = 1;
+        }
+        if (!limit) {
+            limit = 10;
+        }
+        const voluntaryList = await Voluntary.find()
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .populate("ngoDetailId")
+            .exec();
+        const totalCount = await Voluntary.countDocuments();
+        return res.status(200).json({
+            voluntaryList,
+            totalPages: Math.ceil(totalCount / limit),
+            currentPage: page,
+        });
     } catch (err) {
         next(err);
     }
